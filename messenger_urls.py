1ba35c83f6b6b4912b10c16c3a2dddd7df10ca56
dzamakhaiev <dzamakhaiev@star.global>
2024-06-06 15:16:48 +0300

    add requirements.txt add messenger_urls.py

---
+++ messenger_urls.py
@@ -0,0 +1,8 @@
+MESSENGER_HOST = '192.168.50.100'
+MESSENGER_PORT = 5000
+MESSENGER_URL = f'http://{MESSENGER_HOST}:{MESSENGER_PORT}'
+HEALTH = '/health'
+API = '/api'
+LOGIN = API + '/login/'
+USERS = API + '/users/'
+MESSAGES = API + '/messages/'
