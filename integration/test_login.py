30935124061dc0d38333d0a32b841054d61a90a3
dzamakhaiev <dzamakhaiev@star.global>
2024-07-24 17:12:02 +0300

    remove failed test

--- integration/test_login.py
+++ integration/test_login.py
@@ -38,9 +38,6 @@ class LoginTest(TestFramework):
                 self.assertEqual(400, response.status_code, msg=response.text)
                 self.assertEqual('Validation error.', response.text)
 
-    def test_fail(self):
-        self.assertTrue(False)
-
 
 if __name__ == '__main__':
     unittest.main()
