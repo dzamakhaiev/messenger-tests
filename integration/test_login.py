f53a4c0eaa50d17b339a6657554bdc5ca3471a62
dzamakhaiev <dzamakhaiev@star.global>
2024-07-24 15:33:38 +0300

    add failed test

--- integration/test_login.py
+++ integration/test_login.py
@@ -38,6 +38,9 @@ class LoginTest(TestFramework):
                 self.assertEqual(400, response.status_code, msg=response.text)
                 self.assertEqual('Validation error.', response.text)
 
+    def test_fail(self):
+        self.assertTrue(False)
+
 
 if __name__ == '__main__':
     unittest.main()
