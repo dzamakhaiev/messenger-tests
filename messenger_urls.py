29eb1035f87a642ac23e1ab7bff610baaf0f7c9d
dzamakhaiev <dzamakhaiev@star.global>
2024-07-24 12:35:27 +0300

    fix nginx host for ci

--- messenger_urls.py
+++ messenger_urls.py
@@ -1,7 +1,7 @@
 import os
 CI_RUN = int(os.environ.get('CI_RUN', 0))
 
-MESSENGER_HOST = 'localhost' if CI_RUN else '192.168.50.100'
+MESSENGER_HOST = 'nginx-ci' if CI_RUN else '192.168.50.100'
 MESSENGER_PORT = 5001 if CI_RUN else 5000
 MESSENGER_URL = f'https://{MESSENGER_HOST}:{MESSENGER_PORT}'
 HEALTH = '/health'
